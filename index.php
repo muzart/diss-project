92143490cf3dd079bef37196adb761f79d0a7bb0
Muzaffar Artikov <muzaffar@ssdsoftwaresolutions.com>
2021-04-09 17:09:23 +0500

    implement subject choose before testing

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+    $connection = new PDO('mysql:host=localhost;dbname=php_baza','root','');
+
+    if(isset($_SESSION['subject'])) {
+        header('Location: test.php');
+        exit(0);
+    }
+
+    if($_POST) {
+        $subject = (int) $_POST['subject'];
+        $_SESSION['subject'] = $subject;
+        header('Location: test.php');
+        exit(0);
+    }
+
+    $subjects = $connection->query('SELECT * FROM subjects')->fetchAll(PDO::FETCH_ASSOC);
+?>
+<html lang="uz">
+    <head>
+        <title>Fanni tanlash</title>
+        <link rel="stylesheet" href="admin/assets/css/bootstrap.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <h1 class="text-center">Testni boshlash uchun fanni tanlang</h1>
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="subject">Fanlar</label>
+                            <select name="subject" id="subject" class="form-control">
+                                <?php foreach ($subjects as $subject): ?>
+                                <option value="<?= $subject['id'] ?>"> <?= $subject['name'] ?> </option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <p class="text-center">
+                                <input type="submit" value="Boshlash" class="btn btn-primary">
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
